b9f84abb8151079b548e99d331862c697ae6de79
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-23 20:36:43 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,45 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [newData, setNewData] = useState("");
+  const [messages, setMessages] = useState("");
+
+  const handleNameInput = (event) => {
+    const newName = event.currentTarget.value;
+    setName(newName);
+  };
+
+  const handleMessageInput = (event) => {
+    const newMessage = event.currentTarget.value;
+    setMessage(newMessage);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNewData(`name: ${name}\nmessage\n${message}`);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter your name" />
-        <textarea placeholder="Enter your message"></textarea>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onInput={handleNameInput}
+          value={name}
+        />
+        <textarea
+          placeholder="Enter your message"
+          onInput={handleMessageInput}
+          value={message}
+        ></textarea>
         <button type="submit">Enter</button>
       </form>
       <div>
         <button type="button">Update Messages</button>
-        <p></p>
+        <p>{messages}</p>
       </div>
     </div>
   );
