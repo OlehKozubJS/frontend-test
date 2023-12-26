414f9989bdcd40b80013122cf8d2f052fb6772f2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-26 18:42:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { MessageForm } from "./MessageForm";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
-  const [newData, setNewData] = useState({ name, message });
   const [messages, setMessages] = useState([]);
-
-  const handleNameInput = (event) => {
-    const newName = event.currentTarget.value;
-    setName(newName);
-  };
-
-  const handleMessageInput = (event) => {
-    const newMessage = event.currentTarget.value;
-    setMessage(newMessage);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setNewData({ name, message });
-  };
 
   useEffect(() => {
     const getMessages = async () => {
@@ -37,34 +20,9 @@ const App = () => {
     getMessages();
   }, [newData]);
 
-  useEffect(() => {
-    const addNewMessage = async () => {
-      try {
-        await axios.post("http://localhost:3000/save", newData);
-      } catch (error) {
-        console.log(error.message);
-        throw error;
-      }
-    };
-    addNewMessage();
-  }, [newData]);
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          onInput={handleNameInput}
-          value={name}
-        />
-        <textarea
-          placeholder="Enter your message"
-          onInput={handleMessageInput}
-          value={message}
-        ></textarea>
-        <button type="submit">Enter</button>
-      </form>
+      <MessageForm />
       <div>
         <ul>
           {messages.map((message, messageIndex) => {
