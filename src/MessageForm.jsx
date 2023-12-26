4935704d5b1ba57f2fb60ffaa6dc130ed7ff9dee
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-26 13:31:20 +0200

    Create MessageForm.jsx

---
+++ src/MessageForm.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const MessageForm = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit({ name, message });
+  };
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/load");
+        const data = await response.data;
+        setMessages(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error.message);
+        throw error;
+      }
+    };
+    getMessages();
+  }, [handleSubmit]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onInput={handleNameInput}
+        value={name}
+      />
+      <textarea
+        placeholder="Enter your message"
+        onInput={handleMessageInput}
+        value={message}
+      ></textarea>
+      <button type="submit">Enter</button>
+    </form>
+  );
+};
