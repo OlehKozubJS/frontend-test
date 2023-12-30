dfc31af1325143fbc193e7c00a8cbc9648df4c4e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-30 18:24:55 +0200

    Update MessageIn.jsx

--- src/MessageIn.jsx
+++ src/MessageIn.jsx
@@ -1,6 +1,37 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const MessageIn = () => {};
+const MessageIn = () => {
+  const [text, setText] = useState("");
+  const [request, setRequest] = useState({ text: "" });
+
+  const handleInput = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setRequest({ text: request });
+  };
+
+  useEffect(() => {
+    const saveData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/save", request);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    };
+    saveData();
+  }, [request]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea cols="30" rows="10" onInput={handleInput}></textarea>
+      <button type="submit"></button>
+    </form>
+  );
+};
 
 export { MessageIn };
